e0125dd55defac7cb4976b0cefad472eb62f5714
zainabnoor2 <128997422+zainabnoor2@users.noreply.github.com>
2023-05-14 18:51:29 +0500

    Update Main.java

--- Software.E.Project/src/com/java/Main.java
+++ Software.E.Project/src/com/java/Main.java
@@ -3,7 +3,7 @@ package com.java;
 public class Main {
 
 	public static void main(String[] args) {
-		system.out.println("This is Muskan And Zainab,s  Project")
+		System.out.println("This is Muskan And Zainab's Project")
 		System.out.println("This program is used to find square root");
 		source.sourceMethod();
 	}
