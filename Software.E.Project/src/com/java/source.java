7e2e931778f75e33a0e9d07f021afbd9163cc646
zainabnoor2 <128997422+zainabnoor2@users.noreply.github.com>
2023-05-14 18:52:27 +0500

    Update source.java

--- Software.E.Project/src/com/java/source.java
+++ Software.E.Project/src/com/java/source.java
@@ -2,13 +2,9 @@ package com.java;
 
 public class source {
 
-	public static void sourceMethod() {
-		
+	public static void sourceMethod() {	
 		 int a=7,b;
-         b=a*a;
-              System.out.println("Square of "+a+" is "+b);
-              
-		
+                 b=a*a;
+                 System.out.println("Square of "+a+" is "+b);	
 	}
-
-}
+    }
