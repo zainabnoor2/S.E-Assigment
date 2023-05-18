581640ae0a1cabf59f652c4162271129fbffcecb
muskan115 <128500211+muskan115@users.noreply.github.com>
2023-05-18 23:44:38 +0500

    Update Main.java

--- Software.E.Project/src/com/java/Main.java
+++ Software.E.Project/src/com/java/Main.java
@@ -3,7 +3,7 @@ package com.java;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("This is Muskan And Zainab's Project")
+		System.out.println("This is Muskan (77) And Zainab's (120) Project")
 		System.out.println("This program is used to find square root");
 		source.sourceMethod();
 	}
